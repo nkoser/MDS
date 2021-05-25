419fd7ef09f19fa657c618f074be3799f50ed1b7
niklaskoser <nc.koser@web.de>
2021-05-25 14:48:08 +0200

    rfdfddfd

--- auto_conv1d_encoder.py
+++ auto_conv1d_encoder.py
@@ -4,30 +4,133 @@ from tensorflow.keras import layers
 import math
 
 
-class Autoencoder_Conv1D(Model):
+class Autoencoder_Conv_80(Model):
 
     def __init__(self, input_shape, latent_dim):
         self.num_neurons = math.ceil(((input_shape[1] / 2) / 2) / 2)
-        super(Autoencoder_Conv1D, self).__init__()
+        super(Autoencoder_Conv_80, self).__init__()
         self.encoder = tf.keras.Sequential([
             layers.Conv1D(kernel_size=3, filters=32, activation='relu', padding='SAME'),
             layers.MaxPool1D(2, padding='SAME'),
             layers.Conv1D(kernel_size=3, filters=16, activation='relu', padding='SAME'),
             layers.MaxPool1D(2, padding='SAME'),
-            layers.Conv1D(kernel_size=3, filters=8, activation='relu', padding='SAME'),
+            layers.Conv1D(kernel_size=3, filters=1, activation='relu', padding='SAME'),
             layers.MaxPool1D(2, padding='SAME'),
             layers.Flatten(),
             layers.Dense(units=latent_dim)
         ])
         self.decoder = tf.keras.Sequential([
-            layers.Dense(units=self.num_neurons),
+            layers.Dense(10),
             layers.Reshape(target_shape=(-1, 1)),
-            layers.Conv1D(kernel_size=3, filters=8, activation='relu', padding='SAME'),
             layers.UpSampling1D(size=2),
-            layers.Conv1D(kernel_size=2, filters=16, activation='relu', ) if input_shape[1] == 268
-            else layers.Conv1D(kernel_size=3, filters=16, activation='relu', padding='SAME'),
+            layers.Conv1D(kernel_size=3, filters=16, activation='relu', padding="SAME"),
             layers.UpSampling1D(size=2),
             layers.Conv1D(kernel_size=3, filters=32, activation='relu', padding='SAME'),
+            layers.UpSampling1D(size=2),
+            layers.Conv1D(kernel_size=3, filters=3, activation='relu', padding='SAME'),
+        ])
+
+    def call(self, x):
+        encoded = self.encoder(x)
+        self.encoder.summary()
+        decoded = self.decoder(encoded)
+        self.decoder.summary()
+        return decoded
+
+
+class Autoencoder_Conv_200(Model):
+
+    def __init__(self, input_shape, latent_dim):
+        self.num_neurons = math.ceil(((input_shape[1] / 2) / 2) / 2)
+        super(Autoencoder_Conv_200, self).__init__()
+        self.encoder = tf.keras.Sequential([
+            layers.Conv1D(kernel_size=3, filters=32, activation='relu', padding='SAME'),
+            layers.MaxPool1D(2, padding='SAME'),
+            layers.Conv1D(kernel_size=3, filters=16, activation='relu', padding='SAME'),
+            layers.MaxPool1D(2, padding='SAME'),
+            layers.Conv1D(kernel_size=3, filters=1, activation='relu', padding='SAME'),
+            layers.MaxPool1D(2, padding='SAME'),
+            layers.Flatten(),
+            layers.Dense(units=latent_dim)
+        ])
+        self.decoder = tf.keras.Sequential([
+            layers.Dense(25),
+            layers.Reshape(target_shape=(-1, 1)),
+            layers.UpSampling1D(size=2),
+            layers.Conv1D(kernel_size=3, filters=16, activation='relu', padding='SAME'),
+            layers.UpSampling1D(size=2),
+            layers.Conv1D(kernel_size=3, filters=32, activation='relu', padding='SAME'),
+            layers.UpSampling1D(size=2),
+            layers.Conv1D(kernel_size=3, filters=3, activation='relu', padding='SAME'),
+        ])
+
+    def call(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+
+class Autoencoder_Conv_268(Model):
+
+    def __init__(self, input_shape, latent_dim):
+        self.num_neurons = math.ceil(((input_shape[1] / 2) / 2) / 2)
+        super(Autoencoder_Conv_268, self).__init__()
+        self.encoder = tf.keras.Sequential([
+            layers.Conv1D(kernel_size=3, filters=32, activation='relu', padding='SAME'),
+            layers.MaxPool1D(2, padding='SAME'),
+            layers.Conv1D(kernel_size=3, filters=16, activation='relu', padding='SAME'),
+            layers.MaxPool1D(2, padding='SAME'),
+            layers.Conv1D(kernel_size=3, filters=1, activation='relu', padding='SAME'),
+            layers.MaxPool1D(2, padding='SAME'),
+            layers.Flatten(),
+            layers.Dense(units=latent_dim)
+        ])
+        self.decoder = tf.keras.Sequential([
+            layers.Dense(34),
+            layers.Reshape(target_shape=(-1, 1)),
+            layers.UpSampling1D(size=2),
+            layers.Conv1D(kernel_size=2, filters=16, activation='relu', ),
+            layers.UpSampling1D(size=2),
+            layers.Conv1D(kernel_size=3, filters=32, activation='relu', padding='SAME'),
+            layers.UpSampling1D(size=2),
+            layers.Conv1D(kernel_size=3, filters=3, activation='relu', padding='SAME'),
+        ])
+
+    def call(self, x):
+        encoded = self.encoder(x)
+        decoded = self.decoder(encoded)
+        return decoded
+
+class Autoencoder_Conv_800(Model):
+
+    def __init__(self, input_shape, latent_dim):
+        self.num_neurons = math.ceil(((input_shape[1] / 2) / 2) / 2)
+        super(Autoencoder_Conv_800, self).__init__()
+        self.encoder = tf.keras.Sequential([
+            layers.Conv1D(kernel_size=3, filters=32, activation='relu', padding='SAME'),
+            layers.MaxPool1D(2, padding='SAME'),
+            layers.Conv1D(kernel_size=3, filters=64, activation='relu', padding='SAME'),
+            layers.MaxPool1D(2, padding='SAME'),
+            layers.Conv1D(kernel_size=3, filters=32, activation='relu', padding='SAME'),
+            layers.MaxPool1D(2, padding='SAME'),
+            layers.Conv1D(kernel_size=3, filters=16, activation='relu', padding='SAME'),
+            layers.MaxPool1D(2, padding='SAME'),
+            layers.Conv1D(kernel_size=3, filters=1, activation='relu', padding='SAME'),
+            layers.MaxPool1D(2, padding='SAME'),
+            layers.Flatten(),
+            layers.Dense(units=latent_dim)
+        ])
+        self.decoder = tf.keras.Sequential([
+            layers.Dense(25),
+            layers.Reshape(target_shape=(-1, 1)),
+            layers.UpSampling1D(size=2),
+            layers.Conv1D(kernel_size=3, filters=16, activation='relu', padding='SAME'),
+            layers.UpSampling1D(size=2),
+            layers.Conv1D(kernel_size=3, filters=32, activation='relu', padding='SAME'),
+            layers.UpSampling1D(size=2),
+            layers.Conv1D(kernel_size=3, filters=32, activation='relu', padding='SAME'),
+            layers.UpSampling1D(size=2),
+            layers.Conv1D(kernel_size=3, filters=64, activation='relu', padding='SAME'),
             layers.UpSampling1D(size=2),
             layers.Conv1D(kernel_size=3, filters=3, activation='relu', padding='SAME'),
         ])
--- preprocessing.py
+++ preprocessing.py
@@ -5,17 +5,20 @@ import cv2
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+from sklearn.manifold import Isomap
 from tensorflow.keras.applications.vgg16 import VGG16
 from tensorflow.keras.layers import Dense, Activation, Flatten, Dropout
 from plot_keras_history import plot_history
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from tensorflow.keras import layers, losses
 from tensorflow.keras.models import Model
+from tensorflow.python.keras.layers import MaxPooling1D
 from tensorflow.python.keras.models import Sequential
 from tensorflow.keras.utils import to_categorical
+from tensorflow.python.layers.convolutional import Conv1D
 
-from auto_conv1d_encoder import Autoencoder_Conv1D
+from auto_conv1d_encoder import Autoencoder_Conv_80, Autoencoder_Conv_200, Autoencoder_Conv_268, Autoencoder_Conv_800
 from autoe import Autoencoder
 from plotting import plotting_history_1, customize_axis_plotting
 
@@ -29,72 +32,111 @@ def get_data(base_dir):
     train_path = os.path.join(base_dir, 'training')
     test_path = os.path.join(base_dir, 'testing')
     train_data = [np.load(os.path.join(train_path, f)) for f in os.listdir(train_path)]
-    train_labels = train_data[5]
+    label_idx = np.argmin(np.array([len(i.shape) for i in train_data]))
+    train_labels = train_data[label_idx]
+    del train_data[label_idx]
     test_data = [np.load(os.path.join(test_path, f)) for f in os.listdir(test_path)]
-    test_labels = test_data[5]
-    del train_data[5]
-    del test_data[5]
+    label_idx = np.argmin(np.array([len(i.shape) for i in test_data]))
+    test_labels = test_data[label_idx]
+    del test_data[label_idx]
     return train_data, train_labels, test_data, test_labels
 
 
-def normalize_data(data,dim=2):
+def normalize_data(d, dim=2):
     """
-
-    :param data: numpy array with the data
+    :param dim:
+    :param d: numpy array with the data
     :return: normalize data to 0..1
     """
-    for i in range(data.shape[dim]):
+    for i in range(d.shape[dim]):
         # normalize the data over the channels
-        data[:, :, i] = MinMaxScaler().fit_transform(data[:, :, i])
-    return data
+        d[:, :, i] = MinMaxScaler().fit_transform(d[:, :, i])
+    return d
+
+
+def cropping(d):
+    size = d.shape[1]
+    crop = (size - 80) // 2
+    model = Sequential()
+    model.add(layers.Cropping1D(cropping=crop, ))
+    x = model.predict(d)
+    return x
 
 
 if __name__ == '__main__':
-
     # ******************************************************************************************************************
     # ******************************* preprocessing ********************************************************************
     # ******************************************************************************************************************
 
-    root = r'E:\Dokumente\MDS\bbh\bbh'
+    root = '/Users/niklaskoser/Documents/MDS/bbh'
     train, train_Y, test, test_Y = get_data(root)
-    norm_train_data = [normalize_data(d) for d in train]
-    norm_test_data = [normalize_data(d) for d in test]
+    # cropped_train_data = [cropping(d) for d in train]
+    norm_train_data = [normalize_data(d) for d in train]  # cropped_train_data]
+    # cropped_test_data = [cropping(d) for d in test]
+    norm_test_data = [normalize_data(d) for d in test]  # cropped_test_data]
 
     # ******************************************************************************************************************
     # ******************************* feature extraction ***************************************************************
     # ******************************************************************************************************************
 
-    latent_dim = 10
-    features = np.zeros(shape=(len(norm_train_data), train_Y.shape[0], latent_dim))
+    latent_dim = 5
+    features = np.zeros(
+        shape=(9, train_Y.shape[0], latent_dim))  # (len(norm_train_data), train_Y.shape[0], latent_dim))
     for k, data in enumerate(norm_train_data):
-        autoencoder = Autoencoder_Conv1D(norm_train_data[k].shape, latent_dim)
-        autoencoder.compile(metrics=['accuracy'], optimizer=tf.keras.optimizers.Adam(lr=0.0005),
+        input_size = data.shape[1]
+        autoencoder = Sequential()
+        if input_size == 80:
+            autoencoder = Autoencoder_Conv_80(norm_train_data[k].shape, latent_dim)
+        elif input_size == 200:
+            autoencoder = Autoencoder_Conv_200(norm_train_data[k].shape, latent_dim)
+        elif input_size == 268:
+            autoencoder = Autoencoder_Conv_268(norm_train_data[k].shape, latent_dim)
+        elif input_size == 800:
+            autoencoder = Autoencoder_Conv_800(norm_train_data[k].shape, latent_dim)
+
+        autoencoder.compile(metrics=[tf.keras.metrics.MeanSquaredError()], optimizer=tf.keras.optimizers.Adam(lr=0.001),
                             loss=losses.MeanSquaredError())
         history = autoencoder.fit(data, data,
-                                  epochs=40,
+                                  epochs=10,
                                   shuffle=True,
-                                  batch_size=48,
-                                  # validation_split=0.2,
-                                  # validation_data=(x_test, x_test),
-                                  verbose=1)
+                                  batch_size=16,
+                                  verbose=1,
+                                  validation_split=0.2)
+
         plotting_history_1(history.history,
-                           str(norm_train_data[k].shape[1]) + "_autoencoder_{}_.png".format(k + 1),
+                           str(data.shape[1]) + "_autoencoder_{}_.png".format(k + 1),
                            f=customize_axis_plotting("loss"))
         print(autoencoder.encoder(data).shape)
-        features[k] = autoencoder.encoder(data)
+        features[k] = autoencoder.encoder.predict(data)
+        tf.keras.backend.clear_session()
 
     num_classes = np.unique(train_Y).shape[0]
-    print(features.shape)
+
+    ## featuresr = []
+    ## for k in norm_train_data:
+    ##     embedding = Isomap(n_components=20)
+    ##     X_transformed = embedding.fit_transform(k.reshape(k.shape[0], -1))
+    ##     featuresr.append(X_transformed)
+
+    ## features = np.stack(featuresr)
     features = features.reshape((-1, latent_dim, 9))
     print(features.shape)
 
     model = Sequential()
-    model.add(layers.Conv1D(kernel_size=3, filters=32, activation='relu'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.Conv1D(kernel_size=3, filters=64, activation='relu'))
-    model.add(layers.BatchNormalization())
-    model.add(layers.GlobalAveragePooling1D())
-    model.add(Dense(num_classes, activation='softmax'))
+    model.Dense(256, activation="relu")
+    model.Dense(128, activation="relu")
+    model.Dense(num_classes, activation="softmax")
+    ##model.add(layers.Conv1D(kernel_size=5, filters=16, activation='relu', padding="SAME"))
+    ##model.add(layers.BatchNormalization())
+    ##model.add(layers.MaxPool1D(2))
+    ##model.add(layers.Conv1D(kernel_size=3, filters=64, activation='relu', padding='SAME'))
+    ##model.add(layers.BatchNormalization())
+    ### model.add(layers.MaxPool1D(2))
+    ##model.add(layers.Conv1D(kernel_size=1, filters=128, activation='relu', padding="SAME"))
+    ##model.add(layers.BatchNormalization())
+    ##model.add(layers.Flatten())
+    ##model.add(layers.Dense(128))
+    ##model.add(Dense(num_classes, activation='softmax'))
 
     ## features = features.reshape((-1, 9, 64))
     ## vgg16 = VGG16(include_top=False,
@@ -117,7 +159,7 @@ if __name__ == '__main__':
 
     features = normalize_data(features)
     print(np.max(features))
-    history = model.fit(x=features,
+    history = model.fit(features,
                         y=to_categorical(train_Y),
                         batch_size=16,
                         epochs=200,
@@ -128,6 +170,9 @@ if __name__ == '__main__':
     plotting_history_1(history.history,
                        "final_classifier.png",
                        f=customize_axis_plotting("loss"))
+
+    predictions = model.predict_classes(norm_test_data, verbose=2)
+    print(predictions)
     ## class Feature_NN(Model):
     ##     def __init__(self, input_size):
     ##         self.input_shape = input_size
