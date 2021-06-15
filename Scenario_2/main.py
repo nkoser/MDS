6adcec87a3f2eb969de1fa33fa30ceb1775978d7
niklaskoser <nc.koser@web.de>
2021-06-15 15:21:51 +0200

    aewqdwerew

--- Scenario_2/main.py
+++ Scenario_2/main.py
@@ -3,6 +3,7 @@ from math import cos, sin
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy
 from numpy import math
 from scipy import signal
 from utils import savitzky_golay
@@ -38,7 +39,9 @@ plt.figure()
 df.iloc[:, -1].plot()
 plt.savefig("norm_acc.png")
 
-df['acc_norm_sg'] = savitzky_golay(df['acc_norm'].values, 51, 3)
+print(df['acc_norm'].values)
+
+df['acc_norm_sg'] = savitzky_golay(df['acc_norm'].values, 51, 3) #scipy.signal.medfilt(df['acc_norm'].values, 5) #savitzky_golay(df['acc_norm'].values, 51, 3)
 plt.figure()
 df.iloc[:, -1].plot()
 plt.savefig("norm_acc_sg.png")
@@ -64,7 +67,7 @@ angles = [
         -1)]
 
 for t in range(1, len(df)):
-    g_B_s.append(mu * g_B_s[t - 1] + (1 - mu) * df['acc_norm_sg'].values[t])
+    g_B_s.append(mu * g_B_s[t - 1] + (1 - mu) * df['acc_norm'].values[t])
     R.append(calc_R(g_B_s[-1]))
     angles.append(np.matmul(np.array(R[t]).transpose(), np.expand_dims(df[['wx', 'wy', 'wz']].loc[t].values, -1)))
 
@@ -99,7 +102,7 @@ for i in range(len(angles)):
         x_t.append(x_t[i - 1] + (lambda_t * cos(angles[i])))
         y_t.append(y_t[i - 1] + (lambda_t * sin(angles[i])))
 plt.figure()
-plt.scatter(x_t,y_t)
+plt.plot(x_t, y_t)
 for (x, y) in zip(x_t, y_t):
     print("X: {}, Y: {}".format(x, y))
 plt.savefig("Hallo.png")
--- Scenario_2/utils.py
+++ Scenario_2/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import factorial
 
+
 def savitzky_golay(y, window_size, order, deriv=0, rate=1):
     r"""Smooth (and optionally differentiate) data with a Savitzky-Golay filter.
     The Savitzky-Golay filter removes high frequency noise from data.
