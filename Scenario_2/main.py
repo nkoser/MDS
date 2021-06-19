bb36307b4b5b44ee4be008cd3277343deb3ecfaa
niklaskoser <nc.koser@web.de>
2021-06-19 12:35:39 +0200

    Update Code

--- Scenario_2/main.py
+++ Scenario_2/main.py
@@ -1,15 +1,20 @@
-from math import cos, sin
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
-from numpy import math
+
+from math import cos, sin
 from scipy import signal
 from utils import savitzky_golay
 
 
 def calc_update(t, mu):
+    """
+
+    :param t:
+    :param mu:
+    :return:
+    """
     if t == 0:
         return g_B_s
     else:
@@ -17,10 +22,16 @@ def calc_update(t, mu):
 
 
 def calc_R(g_B_s):
+    """
+    Calculate the Rotation/Basis Matrix R for one epoch
+    :param g_B_s: get the actual g_b' gravity vector
+    :return: the Rotation/Basis Matrix R
+    """
     u_z = g_B_s
     u_x = np.cross(np.array([0, 1, 0]).transpose(), u_z)
     u_y = np.cross(u_z, u_x)
     R = np.array([(u_x / np.linalg.norm(u_x)), (u_y / np.linalg.norm(u_y)), (u_z / np.linalg.norm(u_z))])
+    print(R)
     return R
 
 
@@ -39,61 +50,60 @@ plt.figure()
 df.iloc[:, -1].plot()
 plt.savefig("norm_acc.png")
 
-print(df['acc_norm'].values)
-
-df['acc_norm_sg'] = savitzky_golay(df['acc_norm'].values, 51, 3) #scipy.signal.medfilt(df['acc_norm'].values, 5) #savitzky_golay(df['acc_norm'].values, 51, 3)
+# Smoothing of the normalized accelerator data
+df['acc_norm_sg'] = scipy.signal.medfilt(df['acc_norm'].values,
+                                         5)  # savitzky_golay(df['acc_norm'].values, 51, 3)#savitzky_golay(df['acc_norm'].values, 51, 3) #scipy.signal.medfilt(df['acc_norm'].values, 5) #savitzky_golay(df['acc_norm'].values, 51, 3)
 plt.figure()
 df.iloc[:, -1].plot()
 plt.savefig("norm_acc_sg.png")
 
-peaks, _ = signal.find_peaks(df['acc_norm_sg'].values, height=1.05, )
+# Peaks are found where you expect a step by using a threshold
+idx, peaks = signal.find_peaks(df['acc_norm_sg'].values, height=1.03, )
 
 # **********************************************************************************************************************
 # ******************************************* Gyros ********************************************************************
 # **********************************************************************************************************************
 
+# Plot the gyroscope data
 plt.figure()
 df.iloc[:, 4:7].plot()
 plt.savefig("gyros.png")
 
-g_B_s_t0 = np.array([df['wx'].mean(), df['wy'].mean(), df['wz'].mean()])
+# Take the measurements where the peaks were found
+df = df.iloc[idx].reset_index()
 
+# Calculate the gravity vector g_b' for the first timestep
+g_B_s_t0 = np.array([-0.05, 0.703, 0.69])  # np.array([df['wx'].mean(), df['wy'].mean(), df['wz'].mean()])
 g_B_s = [g_B_s_t0]
 R = [calc_R(g_B_s[0])]
 mu = 0.9
 
+# Calculate the velocity angles for the first timestep
 angles = [
     np.matmul(np.array(R).transpose().squeeze(-1), np.expand_dims(df[['wx', 'wy', 'wz']].loc[0].values, -1)).squeeze(
         -1)]
 
+# Calculate g_B' for each timestep
 for t in range(1, len(df)):
-    g_B_s.append(mu * g_B_s[t - 1] + (1 - mu) * df['acc_norm'].values[t])
+    g_B_s.append(mu * g_B_s[t - 1] + (1 - mu) * df['acc_norm_sg'].values[t])
     R.append(calc_R(g_B_s[-1]))
     angles.append(np.matmul(np.array(R[t]).transpose(), np.expand_dims(df[['wx', 'wy', 'wz']].loc[t].values, -1)))
 
-# angles = [np.sum(np.array(angles[1:t]) * (df['time'].loc[t] - df['time'].loc[t - 1]))
-#          for t, angle in enumerate(angles, start=0)]
-print((df['time'].loc[1] - df['time'].loc[0]))
-print(angles[0])
+# Calculate the estimated turning angle for each epoch
 tmp = [angles[0][-1] * df['time'].loc[0]]
 for t in range(1, len(angles)):
-    # t1 -> Oz_1 * (t1-t0)
-    # t2 -> Oz_1 * (t1-t0) + Oz_2 * (t2-t1)
-    # t3 -> t1 + t2 + Oz_2 * (t3-t2)
-    # .
-    # t_540
     tmp.append(tmp[t - 1] + (angles[t][-1] * (df['time'].loc[t] - df['time'].loc[t - 1])))
 
 angles = tmp
 
-# Plotting :D
-
-lambda_t = 75
+lambda_t = 70
 
 x_t = []
 y_t = []
+
 # angles = [math.degrees(x) for x in angles]
 
+# Calculate the x and y Position for each timestep
 for i in range(len(angles)):
     if i == 0:
         x_t.append(lambda_t * cos(angles[i]))
@@ -101,8 +111,10 @@ for i in range(len(angles)):
     else:
         x_t.append(x_t[i - 1] + (lambda_t * cos(angles[i])))
         y_t.append(y_t[i - 1] + (lambda_t * sin(angles[i])))
+
+# Plot the estimated Track
 plt.figure()
+x_t = np.array(x_t)
+y_t = np.array(y_t)
 plt.plot(x_t, y_t)
-for (x, y) in zip(x_t, y_t):
-    print("X: {}, Y: {}".format(x, y))
-plt.savefig("Hallo.png")
+plt.savefig("Track.png")
